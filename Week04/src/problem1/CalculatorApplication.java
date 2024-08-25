12446285a30e66efc08ab63ff8317bdcffed5eca
kristinaradeva1 <kriradeva@abv.bg>
2024-08-25 17:02:15 +0300

    Week04

---
+++ Week04/src/problem1/CalculatorApplication.java
@@ -0,0 +1,29 @@
+package problem1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalculatorApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root
+                = FXMLLoader.load(getClass().getResource("view-calculator.fxml"));
+
+        Scene scene = new javafx.scene.Scene(root);
+
+        // Set the Window title
+        stage.setTitle("Calculator");
+        stage.sizeToScene();
+        stage.resizableProperty().setValue(Boolean.FALSE);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
